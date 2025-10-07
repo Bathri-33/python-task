6c8fcc3558632c0c8d2cf1ac158aed6f232f81da
bathrinarayanan <bathrinarayanan7264@gmail.com>
2025-10-08 04:15:24 +0530

    first commit

---
+++ bio.py
@@ -0,0 +1,7 @@
+print("my name is Badri")
+print("age-22")
+print("place:Madurai")
+print("Qualification: Bsc(maths)")
+print("DOB:16.09.2003")
+print("work experience:2yrs worked as a part time cashier (Dmart)")
+      
