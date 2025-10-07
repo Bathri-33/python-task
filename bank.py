b648f200bb4fe67d056a69a8c5b95a2e5b38593e
bathrinarayanan <bathrinarayanan7264@gmail.com>
2025-10-08 04:39:48 +0530

    first commit

---
+++ bank.py
@@ -0,0 +1,13 @@
+name=input("enter your name:")
+mail=input("enter your mail:")
+phoneno=int(input("enter your phoneno:"))
+ifsc=int(input("enter your ifsc:"))
+
+
+accno=input("enter accno")
+print(name)
+print(mail)
+print(phoneno)
+print(ifsc)
+print(accno)
+
