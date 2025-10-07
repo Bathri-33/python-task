12e36771dc08de4f0019186f179e3137c1cba2d0
bathrinarayanan <bathrinarayanan7264@gmail.com>
2025-10-08 04:46:32 +0530

    fourth commit

---
+++ 12thmark.py
@@ -0,0 +1,11 @@
+Tamil=int(input("enter your tamil mark:"))
+english=int(input("enter your english mark:"))
+maths=int(input("enter your maths mark:"))
+physis=int(input("enter your physis mark:"))
+chemistery=int(input("enter your chemistery mark:"))
+computerscience=int(input("enter your computerscience mark:"))
+total=Tamil+english+maths+computerscience+physis+chemistery
+percentage=(total/600)*100
+print(Tamil, english, computerscience+physis+chemistery)
+print(total)
+print(percentage)
