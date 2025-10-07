1b8e3da5aa8d40249c5f82e7e10b257940704ae4
bathrinarayanan <bathrinarayanan7264@gmail.com>
2025-10-08 04:43:47 +0530

    third commit

---
+++ 10thmark.py
@@ -0,0 +1,8 @@
+Tamil=int(input("enter your tamil mark:"))
+english=int(input("enter your english mark:"))
+maths=int(input("enter your maths mark:"))
+science=int(input("enter your science mark:"))
+social=int(input("enter your social mark:"))
+total=Tamil+english+maths+science+social
+print(Tamil, english, maths,science, social,)
+print(total)
