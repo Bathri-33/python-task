fc75f6b2535d00a7234ff74ada8639d95d1f9f8d
bathrinarayanan <bathrinarayanan7264@gmail.com>
2025-10-08 04:48:30 +0530

    fifth commit

---
+++ formula.py
@@ -0,0 +1,10 @@
+import math
+r=int(input("enter the radius:"))
+h=int(input("enter the height"))
+print("circle",math.pi*r**2)
+print("cylinder",math.pi*r**2*h)
+print("sphereK",math.pi*r**2*4/3)
+print("cone",math.pi*r**3*1/3)
+
+
+
